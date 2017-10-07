6db73087760aa7086071a18633a1597ed0d5f204
IshitaTakeshi <ishitah.takeshi@gmail.com>
2017-10-07 20:09:45 +0300

    Modify dataset to make sure it works in the case that some facts are supportive each other

--- main.py
+++ main.py
@@ -6,16 +6,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 from truthdiscovery import TruthFinder
 
-# with open("book.txt", "r") as f:
-#     reader = csv.reader(f, delimiter="\t")
-#     lines = [line for line in reader if len(line) == 4]
-# dataframe = pd.DataFrame(lines, columns=["website", "isbn", "object", "fact"])
 
 dataframe = pd.DataFrame([
         ["a", "Einstein", "Special relativity"],
         ["a", "Newton", "Universal gravitation"],
-        ["b", "Einstein", "Special relativity"],
-        ["b", "Galilei", "Heliocentrism"],
+        ["b", "Albert Einstein", "Special relativity"],
+        ["b", "Galileo Galilei", "Heliocentrism"],
         ["c", "Newton", "Special relativity"],
         ["c", "Galilei", "Universal gravitation"],
         ["c", "Einstein", "Heliocentrism"]
@@ -28,18 +24,21 @@ vectorizer = TfidfVectorizer(min_df=1)
 vectorizer.fit(dataframe["fact"])
 
 
-def similarity(f1, f2):
-    V = vectorizer.transform([f1.lower(), f2.lower()])
+def similarity(w1, w2):
+    V = vectorizer.transform([w1, w2])
     v1, v2 = np.asarray(V.todense())
     return np.dot(v1, v2) / (norm(v1) * norm(v2))
 
 
 def implication(f1, f2):
-    return similarity(f1, f2)
+    return similarity(f1.lower(), f2.lower())
 
 
-finder = TruthFinder(implication)
+finder = TruthFinder(implication, dampening_factor=0.8, influence_related=0.6)
 
-dataframe = finder.train(dataframe, 4, 0.9)
+print("Inital state")
+print(dataframe)
+dataframe = finder.train(dataframe)
 
+print("Estimation result")
 print(dataframe)
